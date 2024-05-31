e730d9b0bc8a3f812fb800d4d7f22ababee0075f
Léo Maradan <leo.maradan@bpa-solutions.net>
2024-05-31 13:41:10 +0200

    Fix ESLint

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import jest from 'eslint-plugin-jest';
+import onlyWarn from 'eslint-plugin-only-warn';
+import perfectionist from 'eslint-plugin-perfectionist';
+import perfectionistRecommendedNatural from 'eslint-plugin-perfectionist/configs/recommended-natural';
+import sonarjs from 'eslint-plugin-sonarjs';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+const jestAll = jest.configs['all'].rules;
+
+delete jestAll['jest/unbound-method'];
+delete jestAll['jest/no-conditional-in-test'];
+delete jestAll['jest/prefer-importing-jest-globals'];
+
+const config = [
+  {
+    ignores: ['coverage/**', 'test/**', 'build/**', 'dist/**', '.yarn/**']
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  perfectionistRecommendedNatural,
+  sonarjs.configs.recommended,
+  prettier,
+  {
+    linterOptions: {
+      // noInlineConfig: true,
+      reportUnusedDisableDirectives: 'warn'
+    },
+    plugins: { jest, onlyWarn, perfectionist },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_', ignoreRestSiblings: true }
+      ],
+      'no-console': 'warn'
+    }
+  },
+  {
+    files: ['*.cjs'],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  {
+    files: ['src/**/*.test.ts'],
+    plugins: { jest },
+    rules: {
+      ...jestAll,
+      'jest/no-conditional-in-test': 'off',
+      'jest/prefer-importing-jest-globals': 'off',
+      'sonarjs/no-duplicate-string': 'off'
+    }
+  }
+];
+
+export default config;
